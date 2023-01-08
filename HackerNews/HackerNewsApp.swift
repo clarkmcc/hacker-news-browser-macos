e9de1deadfeae7712b0a724861a9b5c7e0b36b09
Clark McCauley <clarkmccauley@gmail.com>
2023-01-08 15:59:11 -0700

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Clark McCauley on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
